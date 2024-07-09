878979a368537b1a2f9ddef2aed1ff95116b36f6
Karem Darwesh <karemdarwesh1035@gmail.com>
2024-07-09 17:45:30 +0300

    fixing the returned datatype

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 """executing multiple coroutines at the same time with async"""
 import asyncio
+from typing import List
+
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     Executes multiple instances of the `wait_random` coroutine concurrently.
 
