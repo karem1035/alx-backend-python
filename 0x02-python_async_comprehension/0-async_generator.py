2e45d319605b2a2eafe050c0492c7d615185dc15
Karem Darwesh <karemdarwesh1035@gmail.com>
2024-07-11 00:54:08 +0300

    task adding typing

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,9 +2,10 @@
 """Module uses generator to return a random float between 0 and 10."""
 import asyncio
 import random
+from typing import AsyncGenerator
 
 
-async def async_generator():
+async def async_generator() -> AsyncGenerator[float, None]:
     """
     An asynchronous generator that yields random float values.
 
