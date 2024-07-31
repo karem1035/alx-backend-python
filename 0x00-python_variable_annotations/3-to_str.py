609937ef02c856bd9979cd03d1ce01f1c078809f
Karem Darwesh <38433851+karem1035@users.noreply.github.com>
2024-07-31 06:21:23 +0300

    task 3

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-""" Basic annotations - to string  """
+'''basic annottation float to string'''
 
 
 def to_str(n: float) -> str:
-    """ Returns the string representation of the float. """
+    '''return n float to string'''
     return str(n)
