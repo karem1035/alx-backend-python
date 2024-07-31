07c94c0897d538c15b8c781ee99e08f1ee0bf67e
Karem Darwesh <38433851+karem1035@users.noreply.github.com>
2024-07-31 07:36:01 +0300

    Task 7 added annotattions

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -3,6 +3,6 @@
 from typing import Tuple, Union
 
 
-def to_kv(k: str, v: Union[int, float]) -> Tuple[str, Union[int, float]]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     '''return a tuple out of 2 arguments'''
-    return (k, v ** 2)
+    return (k, float(v ** 2))
