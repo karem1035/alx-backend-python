36ceba290bd3c9b841c3f8f523c14c92334faef1
Karem Darwesh <38433851+karem1035@users.noreply.github.com>
2024-07-31 06:18:38 +0300

    Task 2 redo

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
-""" Basic annotations concat """
-
+'''returns the floor of the float'''
 import math
 
 
 def floor(n: float) -> int:
-    """ Returns the floor of the float """
+    '''
+    Returns the foor integer less or to the given float `n`.
+    '''
     return math.floor(n)
